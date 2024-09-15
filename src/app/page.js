f2a5557bfbc55ca7de23a4bb35ecc27d7f92751b
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-15 17:56:22 +0800

    Update background gradient

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import CustomCursor from "@/app/components/Cursor";
 
 export default function Home() {
   return (
-    <div className="grid h-screen w-full cursor-none grid-cols-1 overflow-hidden bg-gradient-to-br from-black to-[#272E36] md:grid-cols-2">
-      <div className=""></div>
+    <div className="grid h-screen w-full cursor-none grid-cols-1 overflow-hidden md:grid-cols-2">
+      <div className="bg-gradient-to-br from-[#272E36] to-[#000000]"></div>
       <div className="grid grid-rows-2 bg-white">
         <div className="bg-[#D5C1B6]"></div>
         <div className=""></div>
