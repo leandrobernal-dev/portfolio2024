56797ad83653b6c9f5577d7e17ebdf271f0ade89
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-15 17:39:02 +0800

    installed plugin for tailwind css autoformatting

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import CustomCursor from "@/app/components/Cursor";
 
 export default function Home() {
-    return (
-        <div className="grid-cols-1 grid md:grid-cols-2 cursor-none h-screen w-full overflow-hidden from-black to-[#272E36] bg-gradient-to-br">
-            <div className=""></div>
-            <div className="bg-white grid grid-rows-2 ">
-                <div className="bg-[#D5C1B6]"></div>
-                <div className=""></div>
-            </div>
-            <CustomCursor />
-        </div>
-    );
+  return (
+    <div className="grid h-screen w-full cursor-none grid-cols-1 overflow-hidden bg-gradient-to-br from-black to-[#272E36] md:grid-cols-2">
+      <div className=""></div>
+      <div className="grid grid-rows-2 bg-white">
+        <div className="bg-[#D5C1B6]"></div>
+        <div className=""></div>
+      </div>
+      <CustomCursor />
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
