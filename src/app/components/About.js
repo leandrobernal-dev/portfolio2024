ac5c872d144f1ca03727b48afc241c08be51a34c
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-26 20:15:47 +0800

    Create About section component

---
+++ src/app/components/About.js
@@ -0,0 +1,23 @@
+export default function About() {
+  return (
+    <section
+      className="section mb-24 leading-relaxed text-zinc-400"
+      id="About"
+      aria-labelledby="about-title"
+    >
+      <h2 className="font-bold text-white md:hidden" id="about-title">
+        About
+      </h2>
+      <p>
+        I started out curious about robotics and computers, but eventually fell
+        in love with web development. I've been freelancing for 3 years now,
+        building and maintaining websites and web applications for my clients.
+      </p>
+      <br />
+      <p>
+        In my spare time, I enjoy 3D printing and modeling, or playing
+        fingerstyle on my acoustic guitar🎸.
+      </p>
+    </section>
+  );
+}
