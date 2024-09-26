8fdae1d912a7b36638ac50ed9e71489926cc5faa
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-26 20:26:00 +0800

    Move sections to different components

--- src/app/page.js
+++ src/app/page.js
@@ -1,192 +1,23 @@
-import Nav from "@/app/components/Nav";
-import { Launch } from "@mui/icons-material";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import Link from "next/link";
-
-const SocialLinks = [
-  {
-    name: "Github",
-    url: "https://github.com/leandrobernal-dev",
-    icon: <GitHubIcon fontSize="small" />,
-  },
-];
+import About from "@/app/components/About";
+import ExperienceCards from "@/app/components/ExperienceCards";
+import Footer from "@/app/components/Footer";
+import Header from "@/app/components/Header";
+import ProjectGrid from "@/app/components/ProjectsGrid";
 
 export default function Home() {
   return (
-    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans text-white md:px-12 md:py-20 lg:px-24 lg:py-0">
-      <div className="lg:flex lg:justify-between lg:gap-6">
-        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
-          <div className="">
-            <div>
-              <h1 className="text-4xl font-black">
-                <Link href={"/"}>Leandro Bernal.</Link>
-              </h1>
-              <br />
-              <h2 className="text-xl font-light">
-                Full Stack Software Engineer
-              </h2>
-              <h3 className="text-xl font-light">
-                <strong>|</strong> Web Developer
-              </h3>
-              <br />
-            </div>
-            <div>
-              <p className="text-zinc-400">
-                I build responsive, scalable, and user-focused web applications
-                with modern full-stack technologies.
-              </p>
-            </div>
+    <div className="flex min-h-screen justify-center py-12 font-sans text-white md:py-20 lg:py-0">
+      <div className="w-full max-w-screen-xl px-6 md:px-16 lg:flex lg:justify-between lg:gap-6">
+        <Header />
 
-            <Nav />
-          </div>
-          <div className="flex items-center">
-            <div className="my-8 flex items-center gap-12">
-              <img
-                className="h-12 w-12 rounded-full"
-                src="https://avatars.githubusercontent.com/u/108369489?v=4"
-                alt=""
-              />
-              {SocialLinks.map(({ name, url, icon }) => (
-                <Link
-                  key={name}
-                  href={url}
-                  target="_blank"
-                  className="flex items-center gap-4 text-sm text-zinc-400 transition-colors hover:text-white"
-                >
-                  {icon}
-                  <p>{name} </p>
-                  <Launch fontSize="small" />
-                </Link>
-              ))}
-              <a
-                href="mailto:bernalleandro.dev@gmail.com"
-                className="rounded-full bg-white p-2 px-4 font-semibold text-black transition-transform duration-300 ease-in-out hover:scale-105"
-              >
-                Contact me
-              </a>
-            </div>
-          </div>
-        </header>
+        <main className="flex flex-col items-center pt-24 lg:w-3/4 lg:py-24">
+          <About />
 
-        <main className="flex flex-col pt-24 lg:w-3/4 lg:py-24">
-          <div
-            className="section mb-24 leading-relaxed text-zinc-400"
-            id="About"
-          >
-            <h3 className="font-bold text-white md:hidden">ABOUT</h3>
-            <p>
-              Started out curious about robotics and computers eventually fell
-              in love with web development. I've been freelancing for 3 years
-              now, building and maintaining websites and web applications of my
-              clients.
-            </p>
-            <br />
-            <p>
-              In my spare time, either I do 3d printing and modelling or play
-              fingerstyle on my acoustic guitar 🎸.
-            </p>
-          </div>
+          <ExperienceCards />
 
-          <div
-            className="section relative mb-24 flex flex-col gap-2"
-            id="Experience"
-          >
-            <h3 className="font-bold text-white md:hidden">EXPERIENCE</h3>
+          <ProjectGrid />
 
-            <div className="flex justify-between gap-4 bg-zinc-900 p-2 transition-transform duration-300 ease-in-out hover:scale-110 md:p-8">
-              <span className="min-w-fit text-zinc-400">2022 — Present</span>
-              <div>
-                <h3 className="text-lg font-bold">
-                  Full Stack Engineer | Freelance - Fiverr
-                </h3>
-                <p className="mt-2 text-sm leading-normal text-zinc-400">
-                  I worked with clients to design and build responsive web
-                  applications from concept to deployment. I utilized front-end
-                  technologies like HTML, CSS, JavaScript, and React, alongside
-                  back-end tools such as Node.js, Express, and databases like
-                  MySQL and MongoDB. I focused on delivering seamless user
-                  experiences, implementing API integrations, and optimizing
-                  performance, while ensuring scalability and security. My work
-                  consistently provided clients with high-quality, reliable
-                  digital solutions tailored to their business goals.
-                </p>
-                <p className="mt-2 text-sm leading-normal text-zinc-400">
-                  I also learned Google Cloud Platform along the way. Helped
-                  clients automate their tasks using Google Apps Script.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="section mb-24 grid h-fit w-full grid-cols-2 gap-2"
-            id="Projects"
-          >
-            <div className="flex flex-col gap-4">
-              <h2 className="text-4xl font-black sm:text-6xl">My Projects.</h2>
-            </div>
-            {[0].map((item, index) => (
-              <div
-                key={item + index}
-                className="aspect-square bg-zinc-900 transition-transform duration-300 hover:scale-110 hover:shadow-lg"
-              ></div>
-            ))}
-          </div>
-
-          <footer className="max-w-md pb-16 text-sm text-zinc-500 sm:pb-0">
-            <p>
-              Design Inspired by{" "}
-              <a
-                href="https://brittanychiang.com/"
-                className="font-medium text-zinc-400 hover:text-zinc-300 focus-visible:text-zinc-300"
-                target="_blank"
-                rel="noreferrer noopener"
-                aria-label="Figma (opens in a new tab)"
-              >
-                Brittany Chiang
-              </a>{" "}
-              and coded in{" "}
-              <a
-                href="https://code.visualstudio.com/"
-                className="font-medium text-zinc-400 hover:text-zinc-300 focus-visible:text-zinc-300"
-                target="_blank"
-                rel="noreferrer noopener"
-                aria-label="Visual Studio Code (opens in a new tab)"
-              >
-                Visual Studio Code
-              </a>{" "}
-              by yours truly. Built with{" "}
-              <a
-                href="https://nextjs.org/"
-                className="font-medium text-zinc-400 hover:text-zinc-300 focus-visible:text-zinc-300"
-                target="_blank"
-                rel="noreferrer noopener"
-                aria-label="Next.js (opens in a new tab)"
-              >
-                Next.js
-              </a>{" "}
-              and{" "}
-              <a
-                href="https://tailwindcss.com/"
-                className="font-medium text-zinc-400 hover:text-zinc-300 focus-visible:text-zinc-300"
-                target="_blank"
-                rel="noreferrer noopener"
-                aria-label="Tailwind CSS (opens in a new tab)"
-              >
-                Tailwind CSS
-              </a>
-              , deployed with{" "}
-              <a
-                href="https://vercel.com/"
-                className="font-medium text-zinc-400 hover:text-zinc-300 focus-visible:text-zinc-300"
-                target="_blank"
-                rel="noreferrer noopener"
-                aria-label="Vercel (opens in a new tab)"
-              >
-                Vercel.
-              </a>
-            </p>
-          </footer>
+          <Footer />
         </main>
       </div>
     </div>
