39ef5abea18abcd01fd96fa18ef6da8efe012ff7
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-27 10:42:09 +0800

    Add custom button

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -55,10 +55,13 @@ export default function Header() {
           ))}
           <a
             href="mailto:bernalleandro.dev@gmail.com"
-            className="my-auto flex items-center gap-1 rounded-full bg-white px-4 py-2 font-semibold text-black transition-transform duration-300 ease-in-out hover:scale-105"
+            className="duration-400 relative inline-flex h-12 overflow-hidden rounded-full p-[1px] transition hover:scale-110 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           >
-            <Email />
-            Contact me
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              <Email fontSize="small" className="mr-1" />
+              Email me
+            </span>
           </a>
         </div>
       </div>
