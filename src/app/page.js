135a71a2a9a83d22c5a7031a1296d5b881337556
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-09-27 10:37:33 +0800

    Remove padding

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import ProjectGrid from "@/app/components/ProjectsGrid";
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center py-12 font-sans text-white md:py-20 lg:py-0">
-      <div className="w-full max-w-screen-xl px-6 md:px-16 lg:flex lg:justify-between lg:gap-6">
+      <div className="w-full max-w-screen-xl px-6 lg:flex lg:justify-between lg:gap-6">
         <Header />
 
         <main className="flex flex-col items-center pt-24 lg:w-3/4 lg:py-24">
