26a9a71a6ed11640bcca6c500608f519dc37a5a0
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-10-07 09:48:40 +0800

    update about details

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -17,17 +17,11 @@ export default function About() {
         </p>
         <div>
           <p>
-            My go to framework is Next.js for both front-end only and full stack
-            applications. Here's why:
-          </p>
-          <p>
-            Whenever I come up with a new idea and start building a personal
-            project, I approach it with the mindset that it could eventually
-            become a business. During the development process, there’s always a
-            point where I can evaluate whether the idea has real potential or if
-            it's not worth pursuing. Next.js allows me to reach that point as
-            quickly and efficiently as possible, making it my go-to framework
-            for testing and iterating on ideas.
+            My go-to framework for both front-end and full-stack applications is
+            Next.js. When I start building a new personal project, Next.js helps
+            me quickly and efficiently bring ideas to life. It allows for rapid
+            testing and iteration, making it perfect for exploring new concepts
+            and refining them along the way.
           </p>
         </div>
         <p>
