7ae2a1fe1d4ef0e73ffda4d98df745591759184b
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-10-20 04:50:08 +0800

    use accent color

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -10,15 +10,15 @@ export default function About() {
       </h2>
       <article className="space-y-4">
         <p>
-          I used to build <strong className="text-cyan-400">robots</strong>, now
-          I build websites 🔨🤖➡️🌐. I've been freelancing for{" "}
-          <strong className="text-cyan-400">3 years</strong> now, building and
+          I used to build <strong className="text-accent">robots</strong>, now I
+          build websites 🔨🤖➡️🌐. I've been freelancing for{" "}
+          <strong className="text-accent">3 years</strong> now, building and
           maintaining websites and web applications for my clients.
         </p>
         <div>
           <p>
             My go-to framework for both front-end and full-stack applications is{" "}
-            <strong className="text-cyan-400">Next.js</strong>. When I start
+            <strong className="text-accent">Next.js</strong>. When I start
             building a new personal project, Next.js helps me quickly and
             efficiently bring ideas to life. It allows for rapid testing and
             iteration, making it perfect for exploring new concepts and refining
