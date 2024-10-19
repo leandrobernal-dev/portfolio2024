b1bba2e3d17733e4dd5c44628da0fad3860d8db8
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-10-20 04:40:51 +0800

    update about section. add accent color to highlight some texts

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -10,25 +10,24 @@ export default function About() {
       </h2>
       <article className="space-y-4">
         <p>
-          I started out curious about <strong>robotics</strong> and{" "}
-          <strong>computers</strong>, but eventually fell in love with{" "}
-          <strong>web development</strong>. I've been freelancing for about{" "}
-          <strong>3 years</strong> now, building and maintaining websites and
-          web applications for my clients.
+          I used to build <strong className="text-cyan-400">robots</strong>, now
+          I build websites 🔨🤖➡️🌐. I've been freelancing for{" "}
+          <strong className="text-cyan-400">3 years</strong> now, building and
+          maintaining websites and web applications for my clients.
         </p>
         <div>
           <p>
             My go-to framework for both front-end and full-stack applications is{" "}
-            <strong>Next.js</strong>. When I start building a new personal
-            project, Next.js helps me quickly and efficiently bring ideas to
-            life. It allows for rapid testing and iteration, making it perfect
-            for exploring new concepts and refining them along the way.
+            <strong className="text-cyan-400">Next.js</strong>. When I start
+            building a new personal project, Next.js helps me quickly and
+            efficiently bring ideas to life. It allows for rapid testing and
+            iteration, making it perfect for exploring new concepts and refining
+            them along the way.
           </p>
         </div>
         <p>
-          In my spare time, I enjoy <strong>3D printing</strong> and{" "}
-          <strong>modeling</strong>, or play <strong>fingerstyle</strong> on my{" "}
-          <strong>acoustic guitar🎸</strong>.
+          In my spare time, I enjoy 3D printing and 3d modeling, or play
+          fingerstyle on my acoustic guitar🎸.
         </p>
       </article>
     </section>
