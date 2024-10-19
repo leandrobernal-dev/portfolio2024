cd7159fbadd1226198d69c8cff05cb5e1fbf74b8
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-10-19 15:53:12 +0800

    add upwork link

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,6 +1,6 @@
 import Nav from "@/app/components/Nav";
 import Link from "next/link";
-import { Email, Launch } from "@mui/icons-material";
+import { OpenInNew } from "@mui/icons-material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 const SocialLinks = [
@@ -34,8 +34,8 @@ export default function Header() {
 
         <Nav />
       </div>
-      <div className="flex items-center">
-        <div className="my-8 flex gap-8">
+      <div className="flex w-full items-center pr-4">
+        <div className="my-8 flex w-full gap-4 text-white md:justify-between md:gap-0">
           <img
             className="h-12 w-12 rounded-full"
             src="https://avatars.githubusercontent.com/u/108369489?v=4"
@@ -46,13 +46,27 @@ export default function Header() {
               key={name}
               href={url}
               target="_blank"
-              className="flex items-center gap-1 text-sm text-zinc-400 transition-colors hover:text-white"
+              className="flex items-center gap-3 text-sm transition-colors hover:text-zinc-400"
             >
               {icon}
               <p>{name} </p>
-              <Launch fontSize="small" />
+              <OpenInNew className="h-4 w-4" />
             </Link>
           ))}
+          <a
+            target="_blank"
+            href="https://www.upwork.com/freelancers/~01a704d9fb8d180d89"
+            className="animate-shimmer flex h-12 items-center justify-center gap-2 rounded-full border border-zinc-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] p-2 px-4 font-medium text-zinc-400 transition duration-300 hover:scale-110 focus:outline-none"
+          >
+            <img
+              src="https://www.upwork.com/favicon.ico"
+              className="w-5 invert"
+              alt=""
+            />
+            Let's Work Together
+            <OpenInNew className="h-4 w-4" />
+          </a>
+
           {/* <a
             href="mailto:bernalleandro.dev@gmail.com"
             className="duration-400 relative inline-flex h-12 overflow-hidden rounded-full p-[1px] transition hover:scale-110 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    animation: {
+      shimmer: "shimmer 2s linear infinite",
+    },
+    keyframes: {
+      shimmer: {
+        from: {
+          backgroundPosition: "0 0",
+        },
+        to: {
+          backgroundPosition: "-200% 0",
+        },
+      },
+    },
     extend: {
       colors: {
         background: "var(--background)",
