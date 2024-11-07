47c085be8bb3a6450fbd9caaf7d440b29ae07393
leandrobernal-dev <108369489+leandrobernal-dev@users.noreply.github.com>
2024-11-07 15:11:23 +0800

    update skills/tech stack section design

--- src/app/components/TechStack.js
+++ src/app/components/TechStack.js
@@ -23,28 +23,22 @@ export default function TechStack() {
     { name: "Next.js", icon: SiNextdotjs, color: "text-black dark:text-white" },
     { name: "TypeScript", icon: SiTypescript, color: "text-blue-600" },
     { name: "Tailwind CSS", icon: SiTailwindcss, color: "text-cyan-500" },
-    { name: "Shadcn UI", icon: SiShadcnui, color: "text-white" },
     { name: "Node.js", icon: SiNodedotjs, color: "text-green-600" },
     { name: "Python", icon: SiPython, color: "text-yellow-500" },
     { name: "GraphQL", icon: SiGraphql, color: "text-pink-600" },
     { name: "MongoDB", icon: SiMongodb, color: "text-green-500" },
-    { name: "Firebase", icon: SiFirebase, color: "text-yellow-500" },
-    { name: "Supabase", icon: SiSupabase, color: "text-emerald-600" },
-    { name: "Google Cloud", icon: SiGooglecloud, color: "text-blue-500" },
-    { name: "AWS Lambda", icon: SiAwslambda, color: "text-orange-500" },
     { name: "Docker", icon: SiDocker, color: "text-blue-500" },
     { name: "Git", icon: SiGit, color: "text-red-500" },
-    { name: "VSCode", icon: SiVisualstudiocode, color: "text-blue-500" },
   ];
 
   return (
     <section className="relative flex w-full flex-col gap-2" id="TechStack">
-      <h3 className="mb-2 font-bold text-white">TECH STACK I USE</h3>
+      <h3 className="mb-2 font-bold text-white">MY TECH STACK</h3>
 
-      <div className="grid grid-cols-4 justify-items-center gap-4 sm:grid-cols-8">
+      <div className="grid grid-cols-2 gap-2 sm:grid-cols-5">
         {technologies.map((tech) => (
-          <div key={tech.name} className="flex flex-col items-center">
-            <tech.icon className={`w-6 ${tech.color} mb-2`} />
+          <div key={tech.name} className="flex w-full gap-2 bg-zinc-800 p-4">
+            <tech.icon className={`text-3xl ${tech.color} `} />
             <span className="text-center text-xs text-gray-600 dark:text-gray-400">
               {tech.name}
             </span>
